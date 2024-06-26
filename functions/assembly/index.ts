e7126e3a3394484f30536fbff6b53c6383a118bc
Jai Radhakrishnan <55522316+jairad26@users.noreply.github.com>
2024-06-26 07:26:14 -0700

    better prompting

--- functions/assembly/index.ts
+++ functions/assembly/index.ts
@@ -51,7 +51,7 @@ export function getEmojiFromString(text: string): string {
 // see examples/textgeneration for explanation
 function generateListForEmoji(text: string): string[] {
   // Prompt trick: ask for a simple JSON object.
-  const instruction = `Write the emoji description. It must precisely follow the sample. Only respond with valid JSON object containing a valid JSON array named 'list', in this format:
+  const instruction = `Write every emoji's description in the same order as given, you can not miss one. It must precisely follow the sample. Only respond with valid JSON object containing a valid JSON array named 'list', in this format:
   {"list":["sobbing face", "red apple"]}
   `;
 
