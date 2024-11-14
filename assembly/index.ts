81ce3e484aefc85c92ec68121711d12923ead763
Kevin Mingtarja <kevinm@hypermode.com>
2024-11-14 08:43:12 -0800

    fix

--- assembly/index.ts
+++ assembly/index.ts
@@ -41,7 +41,8 @@ export function getEmojiFromString(text: string): string {
 // see examples/textgeneration for explanation
 function generateListForEmoji(text: string): string[] {
   // Prompt trick: ask for a simple JSON object.
-  const instruction = `Write the emoji. Use very complicated words. It must precisely follow the sample. Only respond with valid JSON object containing a valid JSON array named 'list'`;
+  const instruction = `Write the emoji. Use very complicated words. It must precisely follow the sample. Only respond with valid JSON object containing a valid JSON array named 'list', in this format:
+  {"list":["😭: sobbing face", "🍎: red apple"]}`;
 
   const model = models.getModel<OpenAIChatModel>(generationModelName);
   const input = model.createInput([
